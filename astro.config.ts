7cdd35c785276dc965e2d108d2b775200365df58
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-05-22 21:30:18 +0200

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, envField } from 'astro/config'
+import { defineConfig, envField, fontProviders } from 'astro/config'
 /// <reference types="deno" />
 
 // https://astro.build/config
@@ -14,5 +14,33 @@ export default defineConfig({
         endsWith: '/'
       })
     }
+  },
+
+  experimental: {
+    fonts: [
+      {
+        name: 'Inter',
+        provider: fontProviders.google(),
+        cssVariable: '--font-inter',
+        weights: [400, 600, 700],
+        subsets: ['latin'],
+        styles: ['normal']
+      },
+      {
+        name: 'Gatsby Gilmour',
+        provider: 'local',
+        cssVariable: '--font-gatsby',
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/gatsby_gilmour/GatsbyGilmour-Regular.woff2',
+              './src/assets/fonts/gatsby_gilmour/GatsbyGilmour-Regular.woff'
+            ]
+          }
+        ]
+      }
+    ]
   }
 })
