3aeadd074203aa59d966dd24dc9ef7a5760bf384
Adrien PROKOPOWICZ <adrien@nuag.fr>
2025-05-23 17:30:58 +0200

    More wip

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,16 +1,46 @@
-﻿<template>
-  <slot />
+<template>
+  <div class="accordion">
+    <div class="accordion__panels">
+      <slot />
+    </div>
+    <div class="accordion__aside">
+      <img :src="images[active]" alt="" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, provide } from 'vue'
 
-const props = defineProps<{ images: Record<string, string>, default: string }>()
+const props = defineProps<{ images: Record<string, string>; default: string }>()
 
 const active = ref(props.default)
 provide('active', active)
 </script>
 
 <style lang="scss">
+.accordion {
+  display: flex;
+  flex-direction: row;
+  margin-top: 60px;
 
+  &__panels {
+    width: 50%;
+    box-sizing: border-box;
+    padding-right: 92px;
+  }
+
+  &__aside {
+    > img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      object-position: center;
+      border-radius: 8px;
+    }
+    pointer-events: none;
+    position: relative;
+    width: 50%;
+  }
+}
 </style>
--- src/components/AccordionTab.vue
+++ src/components/AccordionTab.vue
@@ -1,28 +1,60 @@
-﻿<template>
-  <div class="accordion-tab">
+<template>
+  <div class="accordion-tab" :class="{visible}">
     <h3>{{ headline }}</h3>
     <div class="accordion-tab__content" :class="{ visible }"><slot /></div>
+    <div class="accordion-tab__actions"><slot name="actions" /></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {inject, type Ref, computed} from "vue";
-
-const props = defineProps<{ value: string, headline: string }>()
-const active = inject<Ref<string>>('active')
-
-const visible = computed(() => !!active && props.value === active.value)
+defineProps<{ value: string; headline: string, visible?: boolean }>()
 </script>
 
 <style lang="scss">
-  .accordion-tab {
+@use '../styles/charter';
+@use '../styles/text-styles';
 
-    &__content {
-      opacity: 0;
+.accordion-tab {
 
-      &.visible {
-        opacity: 1;
-      }
+  &.visible {
+    .accordion-tab__content {
+      padding-top: 24px;
+      display: block;
+    }
+    .accordion-tab__actions {
+      display: block;
     }
   }
+
+  & + & {
+    border-top: solid 1px charter.$neutrals400;
+    margin-top: 32px;
+    padding-top: 32px;
+  }
+
+  > h3 {
+    color: charter.$neutrals700;
+    font-family: var(--font-gatsby), sans-serif;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 40px; /* 125% */
+  }
+
+  &__content {
+    display: none;
+    color: charter.$neutrals600;
+
+    font-family: var(--font-inter);
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px; /* 137.5% */
+  }
+
+  &__actions {
+    margin-top: 32px;
+    display: none;
+  }
+}
 </style>
