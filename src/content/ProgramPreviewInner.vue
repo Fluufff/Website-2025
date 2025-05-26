5965b5dfceac6cebacc2ac9048d6cfbdbf271683
Adrien PROKOPOWICZ <adrien@nuag.fr>
2025-05-26 12:05:28 +0200

    Added picture switcher!

---
+++ src/content/ProgramPreviewInner.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Accordion from '../components/Accordion.vue'
+import PictureSwitcher from '../components/PictureSwitcher.vue'
+import { ref } from 'vue'
+
+const currentImage = ref(0)
+</script>
+
+<template>
+  <div class="program_preview">
+    <Accordion alwaysOpen @change="i => currentImage = i">
+      <slot name="accordion" />
+    </Accordion>
+    <PictureSwitcher :current="currentImage">
+      <slot name="pictures" />
+    </PictureSwitcher>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
