98a8cf26ba54b95952d87be1eb211ed3197522d4
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-10-02 19:00:16 +0200

    Lint fixes and updates

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,18 @@
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import astro from 'eslint-plugin-astro'
+import { defineConfig } from 'eslint/config'
 
-export default ts.config(js.configs.recommended, ts.configs.recommended, astro.configs.recommended, {
-  ignores: ['.astro']
-})
+export default defineConfig(
+  js.configs.recommended,
+  ts.configs.recommended,
+  astro.configs.recommended,
+  {
+    ignores: ['.astro']
+  },
+  {
+    rules: {
+      'no-undef': 'off' // Handled by TypeScript
+    }
+  }
+)
