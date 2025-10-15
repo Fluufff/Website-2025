34184151b8270b9e87ad0257fc06f1a5b9240b1e
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-10-15 20:59:52 +0200

    More various fixes

--- src/data/hr/staff_roles.ts
+++ src/data/hr/staff_roles.ts
@@ -1,11 +1,3 @@
 ﻿import type { CollectionEntry } from 'astro:content'
 
 export type StaffRole = CollectionEntry<'staffRoles'>['data']
-
-export function getPageId(role: StaffRole) {
-  function slugize(str: string) {
-    return str.trim().replace(/\s/g, '-').toLowerCase()
-  }
-
-  return slugize(role.department) + '-' + slugize(role.name)
-}
