beed155982c617feeb66596230fe880a9c6bd97f
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-10-29 11:35:13 +0100

    Add loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,21 @@
 import { rolesLoader } from './hr-roles-loader.ts'
 import { loadEnv } from 'vite'
 import process from 'node:process'
+import {
+  scheduleOpenLocationLoader,
+  scheduleTagLoader,
+  scheduleLocationLoader,
+  scheduleEventLoader
+} from './schedule-loader.ts'
 
 const { STRAPI_URI, STRAPI_TOKEN } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '')
 
+const strapiOptions = { uri: STRAPI_URI!, token: STRAPI_TOKEN! }
+
 export const collections = {
-  staffRoles: defineCollection({
-    loader: rolesLoader({
-      uri: STRAPI_URI!,
-      token: STRAPI_TOKEN!
-    })
-  })
+  staffRoles: defineCollection({ loader: rolesLoader(strapiOptions) }),
+  scheduleOpenLocations: defineCollection({ loader: scheduleOpenLocationLoader(strapiOptions) }),
+  scheduleLocations: defineCollection({ loader: scheduleLocationLoader(strapiOptions) }),
+  scheduleEvents: defineCollection({ loader: scheduleEventLoader(strapiOptions) }),
+  scheduleTags: defineCollection({ loader: scheduleTagLoader(strapiOptions) })
 }
