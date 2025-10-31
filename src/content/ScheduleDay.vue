f2764e713d855a98d93cfe281f16b8acca325ae0
Adrien Prokopowicz <adrien.prokpowicz@gmail.com>
2025-10-31 13:44:10 +0100

    Add collapsible event descriptions

--- src/content/ScheduleDay.vue
+++ src/content/ScheduleDay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="schedule__day">
+  <div class="schedule__day" :class="{ jsEnabled }">
     <h2>{{ dayInfo.name }}, {{ dayInfo.short }}</h2>
     <div class="schedule__day__open_locations">
       <div class="schedule__day__open_location" v-for="loc in locationsOfTheDay">
@@ -13,21 +13,14 @@
       </div>
     </div>
     <div class="schedule__day__events">
-      <div class="schedule__day__event" v-for="event in eventsOfTheDay" :key="event.title">
-        <div class="schedule__day__event__time"><IconClock /> {{ displayTime(event.time) }}</div>
-        <h3>{{ event.title }}</h3>
-        <div v-if="event.location !== null" class="schedule__day__event__time">
-          <IconPin /> {{ displayLocation(event.location) }}
-        </div>
-        <div v-if="event.description" class="schedule__day__event__description" v-html="event.description"></div>
-        <ul class="schedule__day__event__tags" v-if="event.tags.length > 0 || event.host">
-          <li v-for="tag in event.tags">
-            <span>{{ displayTagEmoji(tag) }}</span
-            >{{ displayTag(tag) }}
-          </li>
-          <li class="host" v-if="event.host">by {{ event.host }}</li>
-        </ul>
-      </div>
+      <ScheduleEvent
+        v-for="event in eventsOfTheDay"
+        :event="event"
+        :key="event.title"
+        :tags="tags"
+        :locations="locations"
+        :days="days"
+      />
     </div>
   </div>
 </template>
@@ -37,6 +30,8 @@ import type { DayInfo, EventInfo, OpenLocation, OpenTime, TagInfo } from './Sche
 import IconPin from '~icons/brix/pin'
 import IconClock from '~icons/brix/clock'
 import { computed } from 'vue'
+import ScheduleEvent from './ScheduleEvent.vue'
+import { useMounted } from '@vueuse/core'
 
 const props = defineProps<{
   day: number
@@ -47,7 +42,8 @@ const props = defineProps<{
   openLocations: OpenLocation[]
 }>()
 
-const displayLocation = (i: number) => props.locations[i]!
+const jsEnabled = useMounted()
+
 const dayInfo = computed(() => props.days[props.day]!)
 const locationsOfTheDay = computed<OpenLocation[]>(() =>
   props.openLocations.map((l) => ({
@@ -61,9 +57,6 @@ const eventsOfTheDay = computed(() => props.events.filter((e) => e.time[0] === p
 function displayTime(time: OpenTime): string {
   return `${pad(time[1])}:${pad(time[2])} - ${pad(time[3])}:${pad(time[4])}`
 }
-
-const displayTag = (i: number) => props.tags[i]!.name
-const displayTagEmoji = (i: number) => props.tags[i]!.emoji
 
 function pad(num: number): string {
   return num.toString().padStart(2, '0')
@@ -147,63 +140,6 @@ function pad(num: number): string {
     display: flex;
     flex-direction: column;
     gap: 16px;
-  }
-
-  &__event {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    padding: 32px 24px;
-    border-radius: 8px;
-    border: solid 1px charter.$primary200;
-    background: charter.$neutrals100;
-
-    h3 {
-      @include text-styles.heading3;
-
-      @media (max-width: 700px) {
-        @include text-styles.heading4;
-      }
-    }
-
-    &__time {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 8px;
-    }
-
-    &__tags {
-      display: flex;
-      flex-direction: row;
-      list-style: none;
-      margin-left: 0 !important;
-      gap: 8px;
-      border-top: solid 1px charter.$primary200;
-      padding-top: 24px;
-
-      li {
-        display: flex;
-        align-items: center;
-        border-radius: 3px;
-        border: solid 1px charter.$primary200;
-        background: charter.$primary100;
-        gap: 4px;
-        padding: 10px;
-
-        @include text-styles.display2SemiBold;
-        line-height: 10px;
-
-        &.host {
-          margin-left: auto;
-          background: 0;
-          border: 0;
-          @include text-styles.display2Regular;
-          font-style: italic;
-          line-height: 10px;
-        }
-      }
-    }
   }
 }
 </style>
---
+++ src/content/ScheduleEvent.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="schedule__day__event" :key="event.title">
+    <div class="schedule__day__event__time"><IconClock /> {{ displayTime(event.time) }}</div>
+    <h3>{{ event.title }}</h3>
+    <div v-if="event.location !== null" class="schedule__day__event__time">
+      <IconPin /> {{ displayLocation(event.location) }}
+    </div>
+    <button v-if="jsEnabled" :class="{ toggled: descriptionToggled }" @click="descriptionToggled = !descriptionToggled">
+      {{ descriptionToggled ? 'Hide' : 'Show' }} full event description <IconArrow />
+    </button>
+    <div v-if="event.description" class="schedule__day__event__description" :class="{ visible: descriptionToggled }">
+      <div class="schedule__day__event__description__inner" v-html="event.description"></div>
+    </div>
+    <ul class="schedule__day__event__tags" v-if="event.tags.length > 0 || event.host">
+      <li v-for="tag in event.tags">
+        <span>{{ displayTagEmoji(tag) }}</span
+        >{{ displayTag(tag) }}
+      </li>
+      <li class="host" v-if="event.host">by {{ event.host }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMounted } from '@vueuse/core'
+import { ref } from 'vue'
+import IconPin from '~icons/brix/pin'
+import IconClock from '~icons/brix/clock'
+import IconArrow from '~icons/brix/arrow-down-1'
+
+import type { EventInfo, OpenTime, DayInfo, TagInfo } from './Schedule.vue'
+
+const jsEnabled = useMounted()
+const descriptionToggled = ref(false)
+
+const props = defineProps<{ event: EventInfo; days: DayInfo[]; tags: TagInfo[]; locations: string[] }>()
+
+function displayTime(time: OpenTime): string {
+  return `${pad(time[1])}:${pad(time[2])} - ${pad(time[3])}:${pad(time[4])}`
+}
+
+const displayLocation = (i: number) => props.locations[i]!
+const displayTag = (i: number) => props.tags[i]!.name
+const displayTagEmoji = (i: number) => props.tags[i]!.emoji
+
+function pad(num: number): string {
+  return num.toString().padStart(2, '0')
+}
+</script>
+
+<style lang="scss">
+@use '../styles/charter';
+@use '../styles/text-styles';
+
+.schedule__day__event {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 32px 24px;
+  border-radius: 8px;
+  border: solid 1px charter.$primary200;
+  background: charter.$neutrals100;
+
+  h3 {
+    @include text-styles.heading3;
+
+    @media (max-width: 700px) {
+      @include text-styles.heading4;
+    }
+  }
+
+  &__time {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+  }
+
+  &__tags {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    list-style: none;
+    margin-left: 0 !important;
+    gap: 8px;
+    border-top: solid 1px charter.$primary200;
+    padding-top: 24px;
+
+    li {
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      border-radius: 3px;
+      border: solid 1px charter.$primary200;
+      background: charter.$primary100;
+      gap: 4px;
+      padding: 10px;
+
+      @include text-styles.display2SemiBold;
+      line-height: 10px;
+
+      &.host {
+        margin-left: auto;
+        background: 0;
+        border: 0;
+        @include text-styles.display2Regular;
+        font-style: italic;
+        line-height: 10px;
+      }
+    }
+  }
+
+  > button {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 8px;
+    border: 0;
+    background: 0;
+    text-align: left;
+    color: charter.$secondary500;
+    margin-bottom: -32px;
+    cursor: pointer;
+    @include text-styles.display3Regular;
+
+    svg {
+      height: 16px;
+      width: 16px;
+      transition: transform 0.3s ease-out;
+      transform: scale(1, 1);
+    }
+
+    &.toggled svg {
+      transform: scale(1, -1);
+    }
+  }
+
+  &__description {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    height: auto;
+    overflow: hidden;
+
+    &__inner {
+      padding-top: 16px;
+      overflow: hidden;
+    }
+  }
+}
+
+.jsEnabled .schedule__day__event {
+  &__description {
+    display: grid;
+    grid-template-rows: 0fr;
+    opacity: 0;
+    visibility: hidden;
+    transition:
+      grid-template-rows 0.3s ease-out,
+      visibility 0.3s ease-out,
+      opacity 0.3s ease-out;
+
+    &.visible {
+      grid-template-rows: 1fr;
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+}
+</style>
